66e27063ac8893d5303cd96a8574639a81611d99
Brian Tannous <brian.tannous@gmail.com>
2019-04-22 11:08:13 -0500

    Travis docker test

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 debug = True
 max_curr = os.getenv('AMP_MAX', 40)
 min_curr = os.getenv('AMP_MIN', 12)
-sense_user = "email@email.com"
-sense_pass = "password"
-openevse_ip = "1.1.1.1"
+sense_user = os.getenv('SENSE_USER')
+sense_pass =  os.getenv('SENSE_PASS')
+openevse_ip = os.getenv('OPENEVSE_IP')
